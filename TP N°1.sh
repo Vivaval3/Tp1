415c4650c7e579092bb8efa65083c9383f764c5b
luis-fb <lperezj@fi.uba.ar>
2026-03-19 15:58:06 -0300

    Impresión de Elecciones

---
+++ TP N°1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export FILENAME="FILENAME"
+
+while true; do
+  echo "----------------------"
+  echo "1. Crear entorno"
+  echo "2. Correr proceso"
+  echo "3. Listar alumnos"
+  echo "4. Top 10 notas"
+  echo "5. Buscar por padrón"
+  echo "6. Salir"
+  echo "----------------------"
+done
--- Tp1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- while true; do
-   echo 1
-   echo 2
-   echo 3
-   echo 4
-   echo 5
-   echo 6
