f15088d6d0c96d926fd2c47ce92f2f526c2f9820
Gabriel Valentin Villanueva Valenzuela <gvillanueva@fi.uba.ar>
2026-03-19 17:38:13 -0300

    Update TP N°1.sh
    Cambios esteticos al Menu

--- TP N°1.sh
+++ TP N°1.sh
@@ -3,14 +3,15 @@
 export FILENAME="FILENAME"
 
 while true; do
-  echo "----------------------"
-  echo "1. Crear entorno"
-  echo "2. Correr proceso"
-  echo "3. Listar alumnos"
-  echo "4. Top 10 notas"
-  echo "5. Buscar por padrón"
-  echo "6. Salir"
-  echo "----------------------"
+  echo "Menu:"
+  echo "_______________________"
+  echo "1.) Crear entorno"
+  echo "2.) Correr proceso"
+  echo "3.) Listar alumnos"
+  echo "4.) Top 10 notas"
+  echo "5.) Buscar por padrón"
+  echo "6.) Salir"
+  echo "______________________"
 
   read -p "Opción: " opcion
 
