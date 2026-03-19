b9dd9bae94155114a772719928eaf6269d92720f
luis-fb <lperezj@fi.uba.ar>
2026-03-19 16:06:29 -0300

    Opciones del Script

--- TP N°1.sh
+++ TP N°1.sh
@@ -11,4 +11,30 @@ while true; do
   echo "5. Buscar por padrón"
   echo "6. Salir"
   echo "----------------------"
+
+  read -p "Opción: " opcion
+
+  case $opcion in
+    1)
+      echo "Entorno creado."
+      ;;
+    2)
+      echo "Proceso en ejecución..."
+      ;;
+    3)
+      echo "Mostrando alumnos..."
+      ;;
+    4)
+      echo "Top 10 notas..."
+      ;;
+    5)
+      echo "Buscando padrón..."
+      ;;
+    6)
+      echo "Saliendo..."
+      break
+      ;;
+  esac
+
+  echo ""
 done
