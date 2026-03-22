f2c82ad75c1e96f2204ead9489a0ae578faad8b3
jlima-boop <jlima@fi.uba.ar>
2026-03-22 18:04:06 -0300

    a verififcar

--- consolidar.sh
+++ consolidar.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+while true; do
+  for archivo in "$HOME/EPNro1/entrada"/*; do
+    if [-f "$archivo" ];then
+      cat "archivo">> "$HOME/EPNro1/salida/FILENAME"
+      mv "$archivo" "$HOME/EPNro1/salida/procesado/"
+    fi
+  done
+  sleep 2 
+done
+  
