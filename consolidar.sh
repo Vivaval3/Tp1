c1ba2a9f0e9a0d185230f8cfe336afcca7f2d5ba
jlima-boop <jlima@fi.uba.ar>
2026-03-22 18:08:07 -0300

    .
    solo le di un espacio al -f

--- consolidar.sh
+++ consolidar.sh
@@ -2,7 +2,7 @@
 
 while true; do
   for archivo in "$HOME/EPNro1/entrada"/*; do
-    if [-f "$archivo" ];then
+    if [ -f "$archivo" ];then
       cat "archivo">> "$HOME/EPNro1/salida/FILENAME"
       mv "$archivo" "$HOME/EPNro1/salida/procesado/"
     fi
